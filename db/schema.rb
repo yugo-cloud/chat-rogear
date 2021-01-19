70e68add30251cabb3becb10d91788988447b062
yugo-cloud <yu.roadrunner@gmail.com>
2021-01-19 15:43:24 +0900

    ユーザー認証機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2021_01_19_054222) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"

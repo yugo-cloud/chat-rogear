ddbf2d51129f5a1580f6acc470fe661f48ef3090
yugo-cloud <yu.roadrunner@gmail.com>
2021-03-15 12:30:50 +0900

    ActionCableでのメッセージ送受信機能

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,7 +9,12 @@ class MessagesController < ApplicationController
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
     if @message.save
-      ActionCable.server.broadcast "message_channel", content: @message
+      ActionCable.server.broadcast "message_channel", content: {
+        id: @message.id,
+        content: @message.content,
+        user_name: @message.user.name,        
+        created_at: I18n.l(@message.created_at),
+      }
       # redirect_to room_messages_path(@room)
     else
       @messages = @room.messages.includes(:user)
--- app/javascript/channels/message_channel.js
+++ app/javascript/channels/message_channel.js
@@ -3,17 +3,38 @@ import consumer from "./consumer"
 consumer.subscriptions.create("MessageChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
+    console.log("[MessageChannel] connected");
   },
 
   disconnected() {
     // Called when the subscription has been terminated by the server
+    console.log("[MessageChannel] disconnected");
   },
 
   received(data) {
-    const html = `<p>${data.content.text}</p>`;
+    console.log('[MessageChannel] received. data=%o', data);
     const messages = document.getElementById('messages');
     const newMessage = document.getElementById('message_text');
-    messages.insertAdjacentHTML('afterbegin', html);
+    const html = `
+    <div class="message">
+      <div class="upper-message">
+        <div class="message-user">
+          ${ data.content.user_name }
+        </div>
+        <div class="message-date">
+          ${ data.content.created_at }
+        </div>
+      </div>
+      <div class="lower-message">
+        <div class="message-content">
+          ${ data.content.content }
+        </div>
+      </div>
+    </div>
+    `;
+    messages.insertAdjacentHTML('beforeend', html);
+    var bottom = messages.scrollHeight - messages.clientHeight;
+    messages.scroll(0, bottom);
     newMessage.value='';
   }
 });
